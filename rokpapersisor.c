09f664b60134152546dd82ae8c35ec97c433ce7b
Andrew Schineller <66565336+TotallyFarhan@users.noreply.github.com>
2024-02-01 15:42:12 -0500

    finished stuff

--- rokpapersisor.c
+++ rokpapersisor.c
@@ -1,23 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
-
+#include <time.h>
 
 int main() {
-    char playerInput[10];
+
+    srand(time(0));
+
+    int pcChoice = rand() % 3;
+
+    switch (pcChoice) {
+        case 0:
+            printf("Computer chose Rock.\n");
+            break;
+        case 1:
+            printf("Computer chose Paper.\n");
+            break;
+        case 2:
+            printf("Computer chose Scissors.\n");
+            break;
+    }
+
+
+    char playerInput;
     printf("heelo to rock paper sisors \n\n");
     printf("choose either rock paper or scisors \n\n");
-    printf("for example, type 'rock', 'Rock', 'r', or 'R' to type rock\n\n");
+    printf("For example, to input rock, please ONLY type 'R' or 'r'. Nothing else will be accepted. !!!!!\n\n");
+    gotoinput:
     printf("typpe your input: ");
     scanf("%s", &playerInput);
 
-    int pcChoice = rand() % 3;
     int playerChoice;
-    
-    switch (playerInput)
-    {
-        case "Rock" || "rock" || "R" || "r":
+
+    switch (playerInput) {
+        case 'R':
+        case 'r':
+            playerChoice = 0;
+            break;
+        case 'P':
+        case 'p':
             playerChoice = 1;
+            break;
+        case 'S':
+        case 's':
+            playerChoice = 2;
+            break;
+        default:
+            playerChoice = 99;
+            break;
+    }
+
+    if (((playerChoice == 0)&&(pcChoice == 0)) || ((playerChoice == 1)&&(pcChoice == 1)) || ((playerChoice == 2)&&(pcChoice == 2))) {
+        printf("YOU GOT A TIE\n");
+    }
+
+    else if (((playerChoice == 0)&&(pcChoice == 1)) || ((playerChoice == 1)&&(pcChoice == 2)) || ((playerChoice == 2)&&(pcChoice == 0))) {
+        printf("YOU LOST LOL!!!!!!!!!!!!\n");
+    }
+
+    else if (((playerChoice==0)&&(pcChoice==2)) || ((playerChoice==1)&&(pcChoice=0)) || ((playerChoice==2)&&(pcChoice=1))) {
+        printf("YOU WON YAY!!!!!!!!!!!!!!!!!\n");
+    }
+
+    else {
+        goto gotoinput;
     }
 
   return 0;
